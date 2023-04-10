25e66d1593d859a52d4400c662fe9abedbadf566
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-04-10 13:04:19 -0400

    components folder additions

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+    const [movies, setMovies] = useState([
+        {
+            id: 1,
+            title: "The Dark Knight",
+            image:
+                "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+            director: "Christopher Nolan",
+            genre: "Action/Thriller"
+        },
+        {
+            id: 2,
+            title: "The Grand Budapest Hotel",
+            image:
+                "https://m.media-amazon.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@._V1_FMjpg_UX1000_.jpg",
+            director: "Wes Anderson",
+            genre: "Comedy Drama"
+        },
+        {
+            id: 3,
+            title: "Across the Universe",
+            image:
+                "https://i.pinimg.com/originals/8f/8c/11/8f8c1157a9d9d7aafe617343d3b6a6e6.jpg",
+            director: "Julie Taymor",
+            genre: "Musical Drama"
+        }
+    ]);
+
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    if (selectedMovie) {
+        return (
+            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        );
+    }
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((movie) => (
+                <MovieCard
+                    key={movie.id}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />
+            ))}
+        </div>
+    );
+};
