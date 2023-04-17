919f12d7515d768c525927a2ffc08e8c729f7871
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-04-17 12:43:19 -0400

    auth additions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,29 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
     const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
 
     useEffect(() => {
-        fetch("https://my-flix-movies.herokuapp.com/movies")
+        if (!token) {
+            return;
+        }
+
+        fetch("https://my-flix-movies.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` }
+        })
             .then((response) => response.json())
             .then((data) => {
+                console.log(data);
                 const moviesFromApi = data.map((doc) => {
                     return {
                         id: doc._id,
@@ -18,13 +32,11 @@ export const MainView = () => {
                         director: doc.Director['Name'],
                         genre: doc.Genre['Name'],
                     };
-                });
+                }, [token]);
 
                 setMovies(moviesFromApi);
             });
     }, []);
-
-    const [selectedMovie, setSelectedMovie] = useState(null);
 
     if (selectedMovie) {
         return (
@@ -51,4 +63,20 @@ export const MainView = () => {
     );
 };
 
+const [token, setToken] = useState(null);
 
+if (!user) {
+    return (
+        <>
+            <LoginView
+                onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                }} />
+            or
+            <SignUpView />
+        </>
+    );
+}
+
+<button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
