703a739d5590d9bf8beb66cb49c7d74b3c7be0e7
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-04-23 17:16:06 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -75,9 +76,8 @@ export const MainView = () => {
             )}
         </Row>
     );
+
+
+    <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+
 };
-
-
-<button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
-
-
