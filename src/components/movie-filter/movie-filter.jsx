8174a4b19a8897a6ca549dfa77f423b47ef7c381
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-09-11 11:20:42 -0400

    Update movie-filter.jsx

--- src/components/movie-filter/movie-filter.jsx
+++ src/components/movie-filter/movie-filter.jsx
@@ -8,7 +8,7 @@ export const MovieFilter = (props) => {
                 placeholder="Search by Title"
                 value={props.searchTerm}
                 onChange={(e) => props.setSearchTerm(e.target.value)}
-                style={{ width: "30%", padding: "9px", margin: "2px" }} />
+                style={{ width: "30%", padding: "9px", margin: "5px" }} />
         </div>
     )
 }
